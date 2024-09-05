0ffa0a38abeb8162af94700ef1dfc4dc92bf9324
Michael-Meade <mameade@utica.edu>
2024-09-05 16:41:42 -0400

    added ListUsers class. search users on linux and windows

--- lib/snackhack2/list_users.rb
+++ lib/snackhack2/list_users.rb
@@ -1,20 +1,23 @@
 module Snackhack2
-	class ListUsers
-		attr_accessor :user
+  class ListUsers
+    attr_accessor :user
 
-		def initialize
-			@user = user
-		end
-		def linux
-			`cat /etc/passwd`.split("\n").each do |l|
-				puts l.split(":")[0]
-			end
-		end
-		def windows
-			puts `net users`
-		end
-		def windows_search_user
-			puts `net user #{@user}`
-		end
-	end
+    def initialize
+      @user = user
+    end
+
+    def linux
+      `cat /etc/passwd`.split("\n").each do |l|
+        puts l.split(":")[0]
+      end
+    end
+
+    def windows
+      puts `net users`
+    end
+
+    def windows_search_user
+      puts `net user #{@user}`
+    end
+  end
 end
