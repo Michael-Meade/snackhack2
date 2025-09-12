3c0dfc4124e08cc333727f7aaef6ab81706e76c9
Michael-Meade <mameade@utica.edu>
2025-09-11 21:41:39 -0400

    changed the name of the CVE-2017-9841 class.

--- cve.rb
+++ cve.rb
@@ -1,5 +1,6 @@
 require './lib/snackHack2'
-
-php = Snackhack2::Test.new("http://127.0.0.1:3333")
-
+# http://wsptlnuoo3johqzcdlwuj5zcwfh2dwmswz6hahqctuxttvxpanypmwad.onion
+# http://localhost:3333
+php = Snackhack2::CVE20179841.new("http://localhost:3333")
+#php.run
 php.shell
