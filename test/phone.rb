e4879cdba928abebfc386372499c58c56ab38f8b
Michael-Meade <mameade@utica.edu>
2026-02-28 19:45:43 -0500

    fixed PhoneNumbers class. Added more regex in a switch statement which will bunch of different phone number regexs

---
+++ test/phone.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/snackHack2'
+
+d = Snackhack2::PhoneNumber.new
+d.site = "https://pastebin.com/raw/c83VJF1D"
+
+
+#"https://www.hardin.kyschools.us/schools-1/school-phone-numbers"
+
+
+#"https://pastebin.com/raw/c83VJF1D"
+
+#{ "https://www.hardin.kyschools.us/schools-1/school-phone-numbers"
+
+# "https://www.ic3.gov/"
+
+d.run
