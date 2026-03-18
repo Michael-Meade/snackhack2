5b0cb1852da253eaff735eb4d7d7b67a3f7a9f97
Michael-Meade <mameade@utica.edu>
2026-03-17 21:03:34 -0400

    more in gas.txt

---
+++ scripts/ga_threaded.rb
@@ -0,0 +1,33 @@
+require 'parallel'
+require_relative '../lib/snackHack2'
+# Input and Output files
+input_file = "top-1000000-domains.txt"
+output_file = "top_gas2.txt"
+
+
+thread_count = 50
+count = 0
+
+Parallel.each(File.foreach(input_file), in_threads: thread_count) do |line|
+  site = line.strip
+  next if site.empty?
+
+  begin
+    ga = Snackhack2::GoogleAnalytics.new
+    ga.site = "https://#{site}"
+    
+    results = ga.run
+    puts "count: #{count}"
+    g = results.is_a?(Array) ? results.shift : nil
+
+    if g && !g.include?("[+] No Google Analytics found")
+      result_string = "#{g.strip}:#{site}"
+      puts "[FOUND] #{result_string}"
+      
+      File.open(output_file, 'a') { |file| file.puts(result_string) }
+    end
+    count += 1
+  rescue
+    next
+  end
+end
